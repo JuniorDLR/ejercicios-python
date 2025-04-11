b489d8583d7ad027f5495dcdec35649af41b6df7
Laboratorio C201 <labc201@uam.net.ni>
2025-04-11 07:50:54 -0600

    Ejercicios

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+1. Escribe un algoritmo que declare tres variables: una para el nombre de una
+persona, otra para su edad y otra para su estatura. Asigna valores adecuados a cada
+una.
+
+"""
+def input_user():
+    nombre = input("Ingrese un nombre: ")
+    edad = int(input("Ingrese su edad: "))
+    estatura = float(input("Ingrese su estatura: "))
+    
+    return (nombre,edad,estatura)
+
+
+def main():
+    
+    nombre,edad ,estatura = input_user()
+    
+    print("="*15)
+    print("DATOS PERSONALES")
+    print("="*15)
+    print("\n")
+    print(f"Su nombre es: {nombre}\nSu edad es: {edad} \nSu estatura es: {estatura}")
+    
+    
+
+if __name__ == '__main__':
+   main()
